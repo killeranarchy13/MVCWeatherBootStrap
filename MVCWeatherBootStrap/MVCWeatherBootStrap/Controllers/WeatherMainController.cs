6662e43814e88da69914d72a64a7857fc3ea405c
Rohit <rohitsinghdps18@gmail.com>
2016-04-20 11:23:54 +0530

    bootstrap tabs draft

--- MVCWeatherBootStrap/MVCWeatherBootStrap/Controllers/WeatherMainController.cs
+++ MVCWeatherBootStrap/MVCWeatherBootStrap/Controllers/WeatherMainController.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCWeatherBootStrap.HelperClasses;
+using MVCWeatherBootStrap.Models;
 
 namespace MVCWeatherBootStrap.Controllers
 {
     public class WeatherMainController : Controller
     {
-        // GET: WeatherMain
         public ActionResult WeatherMainView()
         {
-            return View();
+            PopulateTheModel obj = new PopulateTheModel();
+            CityWeatherDetails model = new CityWeatherDetails();
+            obj.PopulateTheCityWeatherObject(ref model);
+            return PartialView(model);
         }
 
         public ActionResult TemperatureDetailsView()
         {
             return PartialView();
+        }
+
+        public ActionResult IndexView()
+        {
+            return View();
         }
     }
 }
